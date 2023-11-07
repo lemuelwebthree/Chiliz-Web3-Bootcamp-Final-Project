21eb340d21600470fc78a7f6db88b421d246164a
lemusosa <alvinandwealth@gmail.com>
2023-11-07 07:32:42 +0200

    update useAuth hook

--- virtual-meetup-platform/src/hooks/useAuth.ts
+++ virtual-meetup-platform/src/hooks/useAuth.ts
@@ -1,13 +1,43 @@
 import { useAppContext } from "@/contexts/AppContext";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
 
 export function useAuth() {
+    const { address, isConnected } = useAccount();
+
     const { setAddress, setIsConnected } = useAppContext();
+    const { connect } = useConnect({
+        connector: new InjectedConnector(),
+    });
 
-    const handleConnect = async () => {};
+    const { disconnect } = useDisconnect();
+    const handleConnect = async () => {
+        try {
+            if (isConnected) {
+                await handleDisconnect();
+            }
 
-    const handleDisconnect = async () => {};
+            await connect();
+            setAddress(address ?? "");
+            setIsConnected(isConnected);
+        } catch (e) {
+            console.log("Error connetcing: " + e);
+        }
+    };
+
+    const handleDisconnect = async () => {
+        try {
+            await disconnect();
+            setAddress(address ?? "");
+            setIsConnected(isConnected);
+        } catch (e) {
+            console.log("Error disconnecting: " + e);
+        }
+    };
 
     return {
+        address,
+        isConnected,
         handleConnect,
         handleDisconnect,
     };
